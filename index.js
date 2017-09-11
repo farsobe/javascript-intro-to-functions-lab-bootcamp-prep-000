466147bec40184e3a748e01a58dcabdfea87dcd6
Farrell Sobejana <fsobejana+flatiron@gmail.com>
2017-09-11 13:28:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  /* convert all strings to uppercase letters, then return it */
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  /* convert all strings to lowercase letters, then return it */
+  return string.toLowerCase()
+}
+/*
+function shout(string) {
+   convert all strings to uppercase letters, then prints it
+  var capitalLetters = string.toUpperCase()
+  console.log(capitalLetters);
+}
+
+function whisper(string) {
+  convert all strings to lowercase letters, then prints it
+  var lowercaseLetters = string.toLowerCase()
+  console.log(lowercaseLetters)
+}
+*/
