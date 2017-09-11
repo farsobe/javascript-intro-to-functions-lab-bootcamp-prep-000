e2700454eee1bf05a1bdbb8b64129471eab7525f
Farrell Sobejana <fsobejana+flatiron@gmail.com>
2017-09-11 13:53:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string == string.toLowerCase) {
-    return "I can't hear you!"
+    return "I love you, too"
   } else if (string == string.toUpperCase) {
-    return "YES INDEED"
+    return "I love you, too"
   }
     else {
       return "I love you, too"
