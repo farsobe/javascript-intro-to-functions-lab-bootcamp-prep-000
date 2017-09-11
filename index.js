fcc7443457e509d45205fcb2f29f3eedc348f8d6
Farrell Sobejana <fsobejana+flatiron@gmail.com>
2017-09-11 13:49:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  // returns "I can\'t hear you!" if `string` is lowercase'
   if (string === string.toLowerCase) {
     return "I can't hear you!"
   } else if (string === string.toUpperCase) {
